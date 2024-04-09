4eba9e35720a97c5437793c741dc0ea75d31790d
Duncan Rout <duncanrout@yahoo.com>
2024-04-09 13:56:11 -0500

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
 import './App.css';
-import { Button, CssBaseline, ThemeProvider, Typography } from '@mui/material';
-import { theme } from './theme';
+import { Button, CssBaseline, PaletteMode, ThemeProvider, Typography, createTheme } from '@mui/material';
+import { getDesignTokens } from './themes/theme';
+import React from 'react';
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
+  const [mode, setMode] = React.useState<PaletteMode>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      // The dark mode switch would invoke this method
+      toggleColorMode: () => {
+        setMode((prevMode: PaletteMode) =>
+          prevMode === 'light' ? 'dark' : 'light',
+        );
+      },
+    }),
+    [],
+  );
+
+  // Update the theme only if the mode changes
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline enableColorScheme />
-      <div className="App">
-        <header className="App-header">
-          <h3>drjr website</h3>
-          <Button color='primary' variant='contained'>Example</Button>
-          <Typography variant="body2" color="primary.light">Example light</Typography>
-        </header>
-      </div>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline enableColorScheme />
+        <div className="App">
+          <header className="App-header">
+            <h3>drjr website</h3>
+            <Button color='primary' onClick={colorMode.toggleColorMode} variant='contained'>Example</Button>
+            <Typography variant="body2" color="primary">Example</Typography>
+            <main>Example</main>
+          </header>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
-
 
 export default App;
